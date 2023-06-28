08869e2ee8aa7dffacb9cd35a46c5f615dc0710d
KalaKenyon <100374253+KalaKenyon@users.noreply.github.com>
2023-06-28 18:20:42 -0400

    Add files via upload

---
+++ EveryXCharacter.py
@@ -0,0 +1,8 @@
+def keep_every_third_character(token):
+    result = token[2::3]
+    return result
+
+# Example usage
+token = "ENTERTOKENHERE"
+result = keep_every_third_character(token)
+print(result)
